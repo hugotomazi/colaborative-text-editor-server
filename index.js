f85fa9aea9b01ebe67c50c6bcae7841673894699
Hugo Tomazi Busnardo <hugo.busnardo@gmail.com>
2020-04-06 21:15:52 -0300

    Correção PWA

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 
 
 app.use(express.static('public'))
-app.get('*', (req, res) => res.redirect('https://' + req.headers.host + req.url))
+    // app.get('*', (req, res) => res.redirect('https://' + req.headers.host + req.url))
 app.get('/', (req, res) => res.sendFile(path.resolve('public/index.html')))
 
 let clients = []
